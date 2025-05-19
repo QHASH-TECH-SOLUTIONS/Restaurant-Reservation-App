241449d56d885c0d47268979f71c86d6e13c043d
Aman-Mateen-Zahoor <amman.w4u@gmail.com>
2025-05-19 10:16:22 +0500

    Hi I am Testing my git working

---
+++ src/screens/LocalTest.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LocalTest = () => {
+  return (
+    <View>
+      <Text>LocalTest</Text>
+    </View>
+  )
+}
+
+export default LocalTest
+
+const styles = StyleSheet.create({})
